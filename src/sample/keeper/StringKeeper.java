c63dc8c43c2506d6b4be2923c7f1f40653fb4343
schernov <greyadsl@yandex.ru>
2018-02-08 01:08:30 +0300

    first

---
+++ src/sample/keeper/StringKeeper.java
@@ -0,0 +1,21 @@
+package sample.keeper;
+
+/**
+ * Created by s.chernov on 07.02.2018.
+ */
+public class StringKeeper {
+    public StringKeeper() {
+        this.sb=new StringBuffer();
+    }
+
+    private StringBuffer sb;
+    public void appendInt(int value){
+        sb.append(Integer.toString(value));
+    }
+    public void clearBuffer(){
+        sb.delete(0,sb.length());
+    }
+    public String getString(){
+        return sb.toString();
+    }
+}
